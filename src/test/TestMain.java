3cc5c5b59c87e9b3558383f083069d2398a299c6
jose <joseakd136@gmail.com>
2023-08-09 23:37:08 -0300

    Prueba de insert y otras cosas

--- src/test/TestMain.java
+++ src/test/TestMain.java
@@ -7,39 +7,13 @@ public class TestMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner scanner = new Scanner(System.in);
-        int opcion;
-        boolean continua;
-        String usuario, contrasenia;
-        do {
-        	continua=true;
-            System.out.println("=== Menú de Consola ===");
-            System.out.println("1. Iniciar sesion");
-            System.out.println("2. Salir");
-            System.out.print("Selecciona una opción: ");
-            opcion = scanner.nextInt();
-
-            switch (opcion) {
-                case 1:
-                    System.out.println("Has seleccionado la opcion de iniciar sesion.");
-                    System.out.println("Ingresa el usuario: ");
-                    usuario = scanner.nextLine();
-                    System.out.println("Ingresa la contraseña: ");
-                    contrasenia = scanner.nextLine();
-                    
-                    // Aquí puedes agregar la lógica para la opción 1
-                    break;
-                case 2:
-                	System.out.println("Saliendo del programa.");
-                    continua=false;
-                    // Aquí puedes agregar la lógica para la opción 2
-                    break;
-                default:
-                    System.out.println("Opción inválida. Selecciona una opción válida.");
-                    break;
-            }
-        } while (continua);
-        System.out.println("=== Hasta luego ===");
+		Sucursal sucursal = new Sucursal();
+		sucursal.setId((long) 1);
+		sucursal.setCodigo("015");
+		sucursal.setDireccion("pirulo 123");
+		sucursal.setNombre("BBVA");
+		SucursalController sc= new SucursalController();
+		sc.insert(sucursal);
 	}
 
 }
